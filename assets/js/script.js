deb829919fee35e55c8ae6648aef0394fe765936
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2022-10-31 21:52:02 -0400

    Starting to understand what I need to do - restarted JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-const container = $(".container");
-const time = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+// const container = $(".container");
+// const time = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 // Might need a label/object
 
 // Function to display date on load of page
@@ -9,124 +9,38 @@ window.onload = function() {
   $("#currentDay").html(date);
 }
 
-// function repeatTime () {
-//     const timeblock = ` <div class="row time-block">
-//     <div class="hour col-md-2">9 am</div>
-//     <textarea class="col-md-8"></textarea>
-//     <button class="saveBtn col-md-2"></button>
-//   </div>`
-// }
-
-
-// Getting the current time and knowing how to log them (referenced a Stackoverlow article on the topic)
-function timeValue(current) {
-  let date = new Date();
-  let hour = date.getHours();
-  let minute = date.getMinutes();
-  let amPm = hour >= 12 ? "pm" : "am";
-  hour = hour % 12;
-  hour = hour ? hour : 12;
-  minute = minute < 10 ? "0" +minute : minute;
-  let strTime = hour + amPm;
-  console.log(strTime);
-
-  $.each(time, function(index, value) {
-    console.log(value);
-    $(".time-" + value + " .input").val(localStorage.getItem(value));
-  })
-
-  if (strTime == current) {
-    return $(".present")
-   }
-   if(hour == 12 && hour > parseInt(current) && current.includes('PM')){
-      return $(".future")
-   }
-   if(current.includes('AM') && amPm == 'PM'){
-      return $(".past")
-   } else if(amPm == 'AM' && current.includes('PM')){
-      return $(".future)")
-   }
-   
-   if(current.includes(amPm)){
-      let currentNumber = parseInt(current.replace(amPm, " "))
-      if(currentNumber <= hour){
-         return $(".past")
-      } else if(currentNumber == 12){
-         return $(".past")
-      } else {
-         return $(".future")
-      }
-  }
-}
-
-// if(moment().format('HH') > this[i].id) {
-//   this[i].classList.add("past");
-//   } else if (moment().format('HH') < this[i].id) {
-//     this[i].classList.add("future");
-//   } else {
-//     this[i].classList.add("present");
-//   }
-
-const $(".row");
-
-if(moment().format('HH') > rows[i].id) {
-  rows[i].classList.add("past");
-  } else if (moment().format('HH') < rows[i].id) {
-    rows[i].classList.add("future");
-  } else (moment().format('HH') === rows[i].id) {
-    rows[i].classList.add("present");
+// Function calls localstorage saved information when readying the page
+$(document).ready(function() {
+  const keyStrings = Object.keys(localStorage);
+  for (let i = 0; i < keyStrings.length; i++) {
+    let data = localStorage.getItem(keyStrings[i]);
+    let userInput = $("#" + keyStrings[i]).find("textarea");
+    userInput.val(description);
   }
 
+  // The .on event will log the information into the localStorage
+  $(".saveBtn").on("click", function(e) {
+    e.preventDefault();
+    let description = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
+    localStorage.setItem(time, description);
+  });
 
-// Creating a function that will create the rows and elements
-// $.each(time, function(index, value) {
-//   const bgColor = timeValue(value);
-//   const row = $("<div>").addClass("row");
-//   const timeValueCol = $("<div>" + value + "</div>").addClass("hour time col");
-//   const descriptionCol = $("<div></div").addClass("past description col" + bgColor + " time-" + value);
-//   const saveBtnCol = $("<button></button>").addClass("saveBtn col");
-//   const textarea = $("<textarea></textarea").addClass("input");
-//   const saveIcon = $('<i class="ui-icon-plus"></i>');
-  
-//   row.append(timeValueCol);
-//   row.append(descriptionCol);
-//     descriptionCol.append(textarea);
-//   row.append(saveBtnCol);
-//     saveBtnCol.append(saveIcon);
-
-//   container.append(row);
-// })
-
-// Giving the buttons functionality
-$(".timeBtn").click(function() {
-  const recordTime = $(this).siblings("div.time").text();
-    // console.log(time);
-  const input = $(this).siblings("div.description").children("textarea").val();
-    // console.log(input);
-  localStorage.setItem(recordTime, input);
-})
+  // The following function will organize the different hours into past, present, and future
+  function timeSpan() {
+    let currentTime = moment().function();
+});
 
 
-// function getTime (current) {
-//   const date = new Date();
-//   const hours = date.getHours();
-//   const minutes = date.getMinutes();
-
-//   const amPm = hours >= 12 ? "pm" : "am";
-//   hours = hours % 2;
-//   hours = hours ? hours : 12;
-//   minutes = minutes < 10 ? "0" +minutes : minutes;
-//   let strTime = hours + ":" + minutes + "" + amPm;
-//   console.log(strTime);
-// }
-
-// $(document).ready(repeatTime)
 
 // Additional resources
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
 // https://api.jquery.com/siblings/
 // https://api.jquery.com/val/
 // https://api.jquery.com/each/
 // https://www.w3schools.com/jsref/jsref_getminutes.asp
 // https://www.w3schools.com/jsref/jsref_gettime.asp
+// https://www.w3schools.com/js/js_this.asp
 // https://www.w3schools.com/jsref/jsref_parseint.asp
+// https://learn.jquery.com/using-jquery-core/document-ready/
 // https://stackoverflow.com/questions/8888491/how-do-you-display-javascript-datetime-in-12-hour-am-pm-format
